b05fac4b95503ebdf3209d62fd6d6c0a37d10132
4D4850 <84929757+4D4850@users.noreply.github.com>
2021-08-25 14:02:42 -0500

    Update ALMN.cpp

--- src/simulation/elements/ALMN.cpp
+++ src/simulation/elements/ALMN.cpp
@@ -1,1 +1,42 @@
+#include "simulation/ElementCommon.h"
 
+void Element::Element_ALMN()
+{
+	Identifier = "DEFAULT_PT_ALMN";
+	Name = "ALMN";
+	Colour = PIXPACK(0xC0C0C0);
+	MenuVisible = 1;
+	MenuSection = SC_SOLIDS;
+	Enabled = 1;
+
+	Advection = 0.0f;
+	AirDrag = 0.00f * CFDS;
+	AirLoss = 0.90f;
+	Loss = 0.00f;
+	Collision = 0.0f;
+	Gravity = 0.0f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 0;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 1;
+	Hardness = 1;
+
+	Weight = 100;
+
+	HeatConduct = 153;
+	Description = "Aluminum";
+
+	Properties = TYPE_SOLID|PROP_CONDUCTS|PROP_LIFE_DEC|PROP_HOT_GLOW;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = 933.5f;
+	HighTemperatureTransition = PT_LAVA;
+}
